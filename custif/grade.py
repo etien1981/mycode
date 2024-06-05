fd52fa25a3dedf3d7464f0fe1ba7503cec10a17b
etien1981 <etienfigueredo@gmail.com>
2024-06-05 18:02:40 +0000

    writing custom code

---
+++ custif/grade.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""if, elif, else - A simple program using conditionals to make a decision."""
+
+grade = float(input("What was your assignment grade?"))
+print(grade)
+if grade >= 90:
+    print("Your Grade is A")
+elif grade >= 80 and grade <= 89:
+    print("Your Grade is B")
+elif grade >= 70 and grade <= 79:
+    print("Your Grade is C")
+elif grade >= 60 and grade <= 69:
+    print("Your Grade is D")
+elif grade <= 59:
+    print("You failed")
+else: 
+    print("Invalid Input!")	
