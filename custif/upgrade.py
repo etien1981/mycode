90ad2063644d1c1b716ebf3efef99b4259a8abad
etien1981 <etienfigueredo@gmail.com>
2024-06-05 20:43:04 +0000

    Studying for logic

---
+++ custif/upgrade.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""if, elif, else - Grade Calculator."""
+
+grade = float(input("What was your assignment grade?\n"))
+print(grade)
+if grade >= 90:
+    print("Your Grade is A")
+elif grade >= 80 and grade <= 89:
+    print("Your Grade is B")
+elif grade >= 70 and grade <= 79:
+    print("Your Grade is C")
+elif grade >= 60 and grade <= 69:
+    print("Your Grade is D")
+else: 
+    print("Your grade is F try harder!")	
+	
